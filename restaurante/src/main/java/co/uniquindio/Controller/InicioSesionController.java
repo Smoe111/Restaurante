9203334fc0ade096e3cba9ccf618803f0ca5adee
Sara <131993370+Smoe111@users.noreply.github.com>
2024-04-16 21:42:43 -0500

    .

---
+++ restaurante/src/main/java/co/uniquindio/Controller/InicioSesionController.java
@@ -0,0 +1,29 @@
+package co.uniquindio.Controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class InicioSesionController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private PasswordField txtContraseña;
+
+    @FXML
+    private TextField txtUsuario;
+
+
+}
+
+
